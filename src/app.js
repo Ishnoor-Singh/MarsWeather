4fbfd4c0e71621bf0ae355d2ec6d172adec2a183
Ishnoor-Singh <ishnoors@gmail.com>
2020-02-20 13:53:04 -0800

    done with single sol preview formatting

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var allSolsRouter = require('./routes/allSols');
-var usersRouter = require('./routes/users');
+var oneSolRouter = require('./routes/oneSol');
 
 var app = express();
 
@@ -20,15 +20,15 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', allSolsRouter);
-app.use('/users', usersRouter);
+app.use('/sol', oneSolRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
